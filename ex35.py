a77890695d45a1331bcd5f02870db6a2b3480d36
kristiansumabat <kristian.sumabat@gmail.com>
2017-06-26 17:23:13 +0800

    new exercise

--- ex35.py
+++ ex35.py
@@ -7,7 +7,7 @@ def gold_room():
     if "0" in choice or "1" in choice:
         how_much = int(choice)
     else:
-        dead "Man learn to type a number."
+        dead("Man learn to type a number.")
 
     if how_much < 50:
         print "Nice, you're not greedy"
@@ -23,23 +23,54 @@ def bear_room():
     print "How are you going to move the bear?"
     bear_moved = False
 
-while True:
+    while True:
+        choice = raw_input(">  ")
+
+        if choice == "take honey":
+            dead("The bear looks at you then slaps your face off")
+        elif choice == "taunt bear" and not bear_moved:
+            print "The bear has moved from the door. You can move through it now"
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews off your leg")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+        else:
+            print "I got no idea what that means"
+
+
+def mamba_room():
+    print "here you see the great evil mamba"
+    print "he, it whatever stares at you and you go insane"
+    print "Do you flee for your life or eat your head?"
+
     choice = raw_input(">  ")
 
-    if choice == "take honey":
-        dead("The bear looks at you then slaps your face off")
-    elif choice == "taunt bear" and not bear_moved:
-        print "The bear has moved from the door. You can move through it now"
-        bear_moved = True
-    elif choice == "taint bear" and bear_moved:
-        dead("The bear gets pissed off and chews off your leg")
-    elif choice == "open door" and bear_moved:
-        gold_room()
+    if "flee" in choice:
+        start()
+    elif "head" in choice:
+        dead("Well that was tasty!")
     else:
-        print "I got no idea what that means"
+        mamba_room()
 
-def cthulhu_room():
-    print "here you see the great evil Cthulhu"
-    print "he, it whatever stares at you and you go insane"
-    print
 
+def dead(why):
+    print why, "Good job!"
+    exit(0)
+
+def start():
+    print"you are in a dark room"
+    print "there is a door to your right and left"
+    print "which one do you take?"
+
+    choice = raw_input(">  ")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        mamba_room()
+    else:
+        dead("you stumble around the room until you starve")
+
+
+start()
